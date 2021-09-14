47cab3bb4305a30e5f183abb75b0a0fafd777bbc
Marilise Morona <marymorona@gmail.com>
2021-09-14 17:46:19 -0300

    Updating page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let navBtn = $('.nav-item');
-let bannerSection = $('#mainSlider');
+let bannerSection = $('#carouselExampleInterval');
 let aboutSection = $('#about-area');
 let servicesSection = $('#about-service');
 let contactSection = $('#about-contact');
 
+
+/* Funcionalidades do buttons do menu */
 let scrollTo = '';
 $(navBtn).click(function(){
     let btnId = $(this).attr('id');
@@ -22,5 +24,19 @@ $(navBtn).click(function(){
 
     $([document.documentElement, document.body]).animate({
         scrollTop: $(scrollTo).offset().top - 70
-    }, 1500);
+    }, 1200);
 });
+
+/* Alterar a cor da Navbar quando scrollar a page */
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
+        $('.navbar').css("background-color", "#000000")
+    } else {
+        $('.navbar').css("background-color", "trargba(0, 0, 0, 0.849)")
+    }
+});
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
